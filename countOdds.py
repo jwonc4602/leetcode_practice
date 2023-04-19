a2acdec51964a5b7376a3aa650833fd87a874b99
Jiwon Choi <123442951+jwonc4602@users.noreply.github.com>
2023-04-19 19:35:19 -0400

    Create countOdds.py

---
+++ countOdds.py
@@ -0,0 +1,16 @@
+def countOdds(self, low: int, high: int) -> int:
+  """
+  Given two non-negative integers low and high. Return the count of odd
+  numbers between low and high (inclusive).
+  """
+  count = 0
+  total = (high - low) + 1
+  
+  if total % 2 == 0:
+    count += (total // 2)
+  else:
+    if low % 2 != 0:
+      count += (total // 2) + 1
+    else:
+      count += total // 2
+  return count
